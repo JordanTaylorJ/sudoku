b8c3f569157aa78ae8ddeb9851ff7b017e8a3669
JordanJoseph <jordant.joseph@outlook.com>
2022-10-03 11:14:04 -0600

    add Home component and route to main path /

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import './App.css'; 
 import React from 'react';
-import {BrowserRouter as Router, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Routes>        
+          <Route path='/' element={<Home />}/>
+        </Routes>
       </Router>
     </div>
   );
