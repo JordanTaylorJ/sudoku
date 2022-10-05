e551ca7189ae80d4fba97ece90fc5827506017e3
JordanJoseph <jordant.joseph@outlook.com>
2022-10-05 09:47:43 -0600

    add username and password validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password 
+    validates :username, presence: true
+    validates :username, uniqueness: true 
+    validates :password, presence: true
+    validates :password, length: {in: 3..20}
+
+
 end
