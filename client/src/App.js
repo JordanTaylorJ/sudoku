d2e12b0fe9784b7070fbb17345a77545e9fc4bc0
JordanJoseph <jordant.joseph@outlook.com>
2022-10-05 16:24:21 -0600

    create Game component, add to routes and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Game from './components/Game';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
           <Route path='/' element={<Home user={user} />}/>
           <Route path='/login' element={<Login setUser={setUser} />} />
           <Route path='/signup' element={<Signup setUser={setUser} />} />
+          <Route path='/newgame' element={<Game/>} />
         </Routes>
       </Router>
     </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -41,6 +41,7 @@ const Navbar = ({user, setUser}) => {
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             <Button
                 sx={{ my: 2, color: 'white', display: 'block' }}
+                component={Link} to='/newgame'
             >New Game
             </Button>  
             <Button
