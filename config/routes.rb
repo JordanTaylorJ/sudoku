e8d082db639e458c2c8a8170a9c6f7e7c3095acd
JordanJoseph <jordant.joseph@outlook.com>
2022-10-04 22:37:06 -0600

    add sessions#destroy route and action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/auth', to: "users#show"
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destory'
   
   
   # Routing logic: fallback requests for React Router.
