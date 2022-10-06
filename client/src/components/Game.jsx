027c65bd35a1c39bfc5aca3e0606d1329382c5bf
JordanJoseph <jordant.joseph@outlook.com>
2022-10-06 16:01:03 -0600

    add event listeners to digit tiles and game tiles to select and apply numbers, compare the tile coordinates to the solution coordinates to determine if the answer is correct

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,8 +1,10 @@
-import { SliderValueLabel } from '@mui/material';
-import React from 'react';
+import React, {useState} from 'react';
 
 const Game = () => {
     
+    const [digitSelect, setDigitSelect] = useState('3');
+    const [errors, setErrors] = useState(0);
+
     const startBoard = [
         "--74916-5",
         "2---6-3-9",
@@ -27,27 +29,60 @@ const Game = () => {
     ];
     const digits = [1,2,3,4,5,6,7,8,9];
 
+    function selectNum(e) {
+        let selected = e.target.id
+        setDigitSelect(selected)
+    }
+
+    function selectTile(e){
+        let coordinates = e.target.id
+        let row = coordinates[0]
+        let col = coordinates[1]
+        if(digitSelect === solutionBoard[row][col]){
+            e.target.innerText = digitSelect
+        } else {
+            const newErr = errors
+            setErrors(newErr + 1)
+        }
+    }
+
     let grid = []
     for (let r=0; r<9; r++){
         grid.push([])
         for (let c=0; c<9; c++){
-            console.log(c);
-            grid[r].push(<div className='tile' key={`${c} ${r}`}></div>)
+            grid[r].push(
+                <div 
+                    className='tile' 
+                    key={`${r} ${c}`}
+                    id={`${r}${c}`}
+                    onClick={(e) => selectTile(e)}
+                > 
+                {startBoard[r][c] !== "-" ? startBoard[r][c] : null}
+                </div>
+            )
         }
     }
-    
 
     return(
         <>
-            <h1>Game</h1>
+            <h2>Errors: {errors}</h2>
             <hr width="500px"/>
             <br/>
             <div className='boardStyle'>
                 {grid}
             </div>
+            <br/>
             <div className='digitStyle'>
-                {digits.map((i) => 
-                    <div key={i} className="digitTile" > {i} </div>
+                {digits.map((num) => 
+                    <div 
+                        key={num} 
+                        value={num}
+                        id={num}
+                        className="digitTile" 
+                        onClick={(e) => selectNum(e)}
+                    > 
+                        {num} 
+                    </div>
                 )}
             </div>
         </>
