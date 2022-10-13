91bbe892a8534c990dd9b06824585625be0ddb4d
JordanJoseph <jordant.joseph@outlook.com>
2022-10-13 17:09:07 -0600

    create components for the user info to be read or edited and a button on the user profile to switch between editing and reading the data

---
+++ client/src/components/ReadUserInfo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ReadUserInfo = () => {
+    return(
+        <p>user info here </p>
+    )
+}
+export default ReadUserInfo;
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Login from './Login';
+import ReadUserInfo from "./ReadUserInfo";
+import EditUserInfo from "./EditUserInfo";
 
 const UserProfile = ({user, setUser}) => {
 
+    const [editUser, setEditUser] = useState(false);
+
+    function handleEditUser() {
+        if (editUser === true){
+            setEditUser(false)
+        } else {
+            setEditUser(true)
+        }
+    }
+
+    console.log('are we editing?', editUser)
 
     if (!user) {
         return(
@@ -15,7 +28,12 @@ const UserProfile = ({user, setUser}) => {
                 <h1>Profile</h1>
                 <ul>username: {user.username}</ul>
                 <ul></ul>
-                <button>Edit User Profile</button>
+                {editUser 
+                    ? <EditUserInfo user={user}/>
+                    : <ReadUserInfo user={user}/>
+                }
+                <button onClick={handleEditUser}>Edit User Profile</button>
+
             </>
 
         )
