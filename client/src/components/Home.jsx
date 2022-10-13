a0a9624ac88a6992e4e0dc36835a823bebd5f409
JordanJoseph <jordant.joseph@outlook.com>
2022-10-13 14:22:14 -0600

    add image to home component and css for sizing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import picture from '../richard-bell-xFGRrdyd92E-unsplash.jpg';
 
 const Home = ({user}) => {
     
     if (!user){
-        return <h1>Login to Play!</h1>
+        return(
+            <>
+                <h1>Login to Play!</h1> 
+                <img src={picture} alt="sudoku" class='photo'/>
+            </>
+        )
     } else {
-        return <h1>Welcome {user.username}!</h1>
+        return(
+            <>
+                <h1>Welcome {user.username}!</h1>
+                <img src={picture} alt="sudoku" class='photo'/>
+            </>
+        )
     }
+    
+        
 }
 
 export default Home;
